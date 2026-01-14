9b884ccc942353a0d937a5123dc13ae3e8af6ed7
ab0703 <amarkumarabhinav149@gmail.com>
2026-01-14 18:01:05 +0000

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+import pgzrun
+import random
+
+WIDTH = 800
+HEIGHT = 600
+TITLE = 'sustainable project'
+CENTER_X = WIDTH/2
+CENTRE_Y = HEIGHT/2
+CENTER = (CENTER_X,CENTRE_Y)
+FINAL_LEVEL = 6
+START_SPEED = 10
+ITEMS = ['battery','bottle','chips','plastic']
