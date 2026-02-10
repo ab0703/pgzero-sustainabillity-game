8ff68c8eb388ed73d57a10950938d227287ca07b
ab0703 <amarkumarabhinav149@gmail.com>
2026-02-10 21:04:37 +0000

    game.py

--- game.py
+++ game.py
@@ -50,6 +50,51 @@ def layout_items(items_to_layout):
 
 def animate_items(items_to_animate):
     global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ('center','bottom')
+        animation = animate(item, duration=duration, on_finished = handle_game_over, y = HEIGHT)
+        animations.append(animation)
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.colllidepoint(pos):
+            if 'paper' in item.image:
+                handle_game_completed()
+            else:
+                handle_game_over()
+
+def handle_game_completed():
+    global items, current_level,animations,game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1 
+        items = []
+        animations = []
+
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def draw_text(heading_text,sub_text):
+    screen.draw.text(heading_text,fontsize = 60,center = CENTER, color= 'black')
+    screen.draw.text(sub_text,fontsize = 30, center =(CENTER_X,CENTRE_Y+30),color = 'black')
+
+
+
+
+
+
+
+
 
 
 
